43e5f5123966c16ef5d9b546f8b65b758d8cb48f
Josef Stewart <josefs@brdata.com>
2026-03-18 17:02:10 -0400

    Adjust Program.cs

--- SewingBizApi/obj/Debug/net8.0/SewingBizApi.AssemblyInfo.cs
+++ SewingBizApi/obj/Debug/net8.0/SewingBizApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SewingBizApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f44444b4c20b64d29ac6fc4704fc6dad0028b8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SewingBizApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SewingBizApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
